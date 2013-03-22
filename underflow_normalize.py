8da3ef3ed0a34b0f4de19c0ce3055206a1688518
nipunreddevil <nipunreddevil@gmail.com>
2013-03-22 13:07:35 +0530

    Viterbi algorithm now uses sclaing to avoid underflow

--- underflow_normalize.py
+++ underflow_normalize.py
@@ -1,3 +1,26 @@
 import numpy as np
 
-
+def normalize(A,dim=None):
+    if dim is None:
+        z=sum(A.flatten(1))
+        s=z+(z==0.0)
+        M=A/s
+    elif dim==1:
+        z=np.sum(A)
+        s=z+(z==0.0)
+        M=A/s
+    else:
+        
+        z=np.sum(A,axis=dim-1)
+        print z
+        
+        s = z + (z==0)
+        s=s[:, np.newaxis]
+        M=A/s
+    return [M,s]
+        
+'''      
+c=np.array([[.7,.3, .3],[.4,.6,.4]])
+[M,s]=normalize(c, 2)
+print M,s
+'''
--- viterbi_path.py
+++ viterbi_path.py
@@ -1,6 +1,7 @@
 import numpy as np
+from underflow_normalize import normalize
 
-def path(prior,transition_matrix,emission_matrix,observation_vector):
+def path(prior,transition_matrix,emission_matrix,observation_vector,scaling=True):
     number_of_hidden_states=len(start_probability)
     number_of_observations=len(observation_vector)
     shape_delta=(number_of_hidden_states,number_of_observations)
@@ -15,6 +16,9 @@ def path(prior,transition_matrix,emission_matrix,observation_vector):
     first_observation=observation_vector[0]
     delta[:,0]=prior*emission_matrix[first_observation:,0]
     psi[:,0]=0
+    if scaling:
+        [delta[:,0], n] = normalize(delta[:,0])
+        scale[0] = 1/n;
     
 
     '''2.Recursion'''
@@ -27,7 +31,9 @@ def path(prior,transition_matrix,emission_matrix,observation_vector):
                 if p>delta[j][t]:
                     delta[j][t]=p  
                     psi[j][t]=i 
-     
+        if scaling:
+            [delta[:,t], n] = normalize(delta[:,t]);
+            scale[t] = 1/n;
     
     '''3.Termination'''
     p_star=max(delta[:,number_of_observations-1])
@@ -37,12 +43,12 @@ def path(prior,transition_matrix,emission_matrix,observation_vector):
     for t in range(number_of_observations-2,-1,-1):
         optimum_path[t]=psi[optimum_path[t+1]][t]
     
-    return optimum_path[:500],delta[0][1:550]
+    return optimum_path,delta
     
   
 start_probability=np.array([.6,.4])
 transition_probability=np.array([[.7,.3],[.4,.6]])
 emission_probability=np.array([[.1,.4,.5],[.6,.3,.1]])
 #observed_sequence=np.array([0,1,1,1])
-observed_sequence=np.random.randint(2,size=10000)
+observed_sequence=np.random.randint(2,size=50000)
 print path(start_probability,transition_probability,emission_probability,observed_sequence)
